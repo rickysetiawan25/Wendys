3edfb87f014669b70821c6e56801e6fd6575c454
rickysetiawan25 <132119740+rickysetiawan25@users.noreply.github.com>
2023-04-30 17:51:44 +0700

    Add files via upload

---
+++ estimasi.py
@@ -0,0 +1,28 @@
+import pickle
+import streamlit as sl
+
+model = pickle.load(open('estimasi_Wendys.sav', 'rb'))
+
+sl.title('Estimasi Jumlah Kalori Makanan Di Menu Wendys')
+sl.write('---')
+
+
+Fat = sl.number_input('Input Total Fat')
+SatFat = sl.number_input('Input Total SatFat')
+TransFat = sl.number_input('Input Total TransFat')
+Cholesterol = sl.number_input('Input Total Cholesterol')
+Sodium = sl.number_input('Input Total Sodium')
+TotalCarb = sl.number_input('Input Total TotalCarb')
+DietaryFiber = sl.number_input('Input Total DietaryFiber')
+Sugars = sl.number_input('Input Total Sugars')
+Protein = sl.number_input('Input Total Protein')
+WeightWatchers = sl.number_input('Input Total WeightWatchers')
+
+
+predict = ''
+
+if sl.button('Calories'):
+    predict = model.predict(
+        [[Fat,SatFat,TransFat,Cholesterol,Sodium,TotalCarb,DietaryFiber,Sugars,Protein,WeightWatchers]]
+    )
+    sl.write ('Estimasi jumlah kalori makanan  di Wendys (kCal) : ', predict)
